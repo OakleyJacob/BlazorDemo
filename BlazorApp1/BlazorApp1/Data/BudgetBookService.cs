f9da4da4512cac26568c3ef5b1ef3aa37b4c5803
Jacob Oakley <118224131+OakleyJacob@users.noreply.github.com>
2023-11-24 15:31:24 -0600

    Add files via upload

---
+++ BlazorApp1/BlazorApp1/Data/BudgetBookService.cs
@@ -0,0 +1,22 @@
+﻿using BlazerApp1.Data;
+using System.Security.Cryptography.X509Certificates;
+
+namespace BlazorApp1.Data
+{
+	public class BudgetBookService
+	{
+		private static List<Entry> mockDB = new List<Entry>()
+		{
+			
+		};
+		public async Task<bool> AddEntry(Entry newEntry)
+		{
+			mockDB.Add(newEntry);
+			return true;
+		}
+		public async Task<List<Entry>> GetAllEntries ()
+		{
+			return mockDB;
+		}
+	}
+}
---
+++ BlazorApp1/BlazorApp1/Data/Entry.cs
@@ -0,0 +1,8 @@
+﻿namespace BlazerApp1.Data
+{
+	public class Entry
+	{
+		public string Amount { get; set; }
+		public string Description { get; set; }
+	}
+}
